647db1c921e4fcccf153993a3bcd363e981cd964
Sebastien Cadorette <cado.94@me.com>
2018-01-14 16:50:50 -0500

    SC : Modification done to the getHeroes function in hero service.

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -8,19 +8,47 @@ import { HEROES } from './mock-heroes';
 import {MessageService } from './message.service';
 
 // HTTP part.
-import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable()
 export class HeroService {
-  private heroesUrl = 'api/heroes';
+  private heroesUrl = 'api/heroes';   // URL to the web API.
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+/**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
+  /* Now gets heroes from the server. */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(heroes => this.log(`fetched heroes`)),
+      catchError(this.handleError('getHeroes', []))
+    );
+  }
+
+  /* Log a HeroService message with the MessageSerice. */
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
   }
